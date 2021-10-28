2001338690439300255425b1cfdf6fbf9669887e
Matt Meyers <matt@mattmeyers.dev>
2021-10-28 19:50:28 -0400

    feat: Add temp and persistant scratch pads

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"log"
+	"fmt"
 	"os"
 	"os/exec"
 	"path/filepath"
@@ -10,30 +10,56 @@ import (
 )
 
 func main() {
+	if err := run(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func run() error {
 	defaultDataDir, err := getDefaultDataDir()
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 
 	app := &cli.App{
 		Name:   "scratch",
-		Usage:  "quick notes",
-		Action: handleEditScratchPad,
+		Usage:  "Quick terminal notes",
+		Action: handleEditTmpFile,
 		Flags: []cli.Flag{
 			&cli.StringFlag{
 				Name:    "editor",
 				Value:   "vim",
+				Usage:   "A terminal command to open the desired text editor",
 				Aliases: []string{"e"},
 			},
 			&cli.StringFlag{
 				Name:  "data-dir",
 				Value: defaultDataDir,
+				Usage: "The directory to use for story scratch pads",
+			},
+		},
+		Commands: []*cli.Command{
+			{
+				Name:   "pad",
+				Usage:  "Edit the shared persistant scratch pad",
+				Action: handleEditScratchPad,
+				Flags: []cli.Flag{
+					&cli.BoolFlag{
+						Name:  "fresh",
+						Usage: "Open a fresh scratch pad (this irreversibly deletes the old data)",
+						Value: false,
+					},
+				},
 			},
 		},
 	}
+
 	if err := app.Run(os.Args); err != nil {
-		log.Fatal(err)
+		return err
 	}
+
+	return nil
 }
 
 func getDefaultDataDir() (string, error) {
@@ -49,18 +75,38 @@ func createDirIfNotExists(dir string) error {
 	return os.MkdirAll(dir, 0755)
 }
 
+func handleEditTmpFile(c *cli.Context) error {
+	f, err := os.CreateTemp("", "scratch-")
+	if err != nil {
+		return err
+	}
+	defer os.Remove(f.Name())
+	defer f.Close()
+
+	return editFile(c.String("editor"), f.Name())
+}
+
 func handleEditScratchPad(c *cli.Context) error {
 	if err := createDirIfNotExists(c.String("data-dir")); err != nil {
 		return err
 	}
 
-	return editFile(c.String("editor"), filepath.Join(c.String("data-dir"), "scratch.md"))
+	filename := filepath.Join(c.String("data-dir"), "scratch.md")
+
+	if c.Bool("fresh") {
+		err := os.WriteFile(filename, []byte{}, 0755)
+		if err != nil {
+			return fmt.Errorf("unable to clear old scratch pad: %v", err)
+		}
+	}
+
+	return editFile(c.String("editor"), filename)
 }
 
 func editFile(editor, filename string) error {
 	editorExec, err := exec.LookPath(editor)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 
 	c := exec.Command(editorExec, filename)
